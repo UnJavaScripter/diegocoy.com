efb053f954cbc101f859631d099d481166ea8929
UnJavaScripter <diegocoyt@gmail.com>
2020-05-06 13:14:23 +0200

    update dc-icon to use templates with svg instead of font
    fix: home y-scroll

--- public/assets/components/dc-elements/dc-icon.js
+++ public/assets/components/dc-elements/dc-icon.js
@@ -1,96 +1,49 @@
 class DCIcon extends HTMLElement {
-    constructor() {
-        super();
-        const shadow = this.attachShadow({mode: 'open'});
-        const icon = document.createElement('span');
-        
-        const style = document.createElement('style');
-
-        const iconName = this.getAttribute('icon');
-        this.iconColor = this.getAttribute('icon-color') || '#000';
-
-        icon.setAttribute('class', 'dc-icon-' + iconName);
-        icon.setAttribute('aria-label', iconName + " logo");
-
-        style.textContent = this.style();
-        
-        shadow.appendChild(style);
-        shadow.appendChild(icon);
-
-    }
-
-    style () {
-        return `
-            :host(:hover) [class^="dc-icon-"] {
-                color: ${this.iconColor};
-                filter: grayscale(0);
-            }
-            [class^="dc-icon-"] {
-                /* use !important to prevent issues with browser extensions that change fonts */
-                font-family: 'icomoon' !important;
-                speak: none;
-                font-style: normal;
-                font-weight: normal;
-                font-variant: normal;
-                text-transform: none;
-                line-height: 1;
-            
-                /* Better Font Rendering =========== */
-                -webkit-font-smoothing: antialiased;
-                -moz-osx-font-smoothing: grayscale;
-
-                font-size: 5rem;
-
-                color: #444;
-
-                filter: grayscale(1);
-                transition: filter 0.5s, color 0.4s;
-            }
-            
-            .dc-icon-file-text2:before {
-                content: "\\e926";
-            }
-            .dc-icon-location2:before {
-                content: "\\e948";
-            }
-            .dc-icon-list:before {
-                content: "\\e9ba";
-            }
-            .dc-icon-link:before {
-                content: "\\e9cb";
-            }
-            .dc-icon-cross:before {
-                content: "\\ea0f";
-            }
-            .dc-icon-share:before {
-                content: "\\ea7d";
-            }
-            .dc-icon-embed2:before {
-                content: "\\ea80";
-            }
-            .dc-icon-terminal:before {
-                content: "\\ea81";
-            }
-            .dc-icon-mail:before {
-                content: "\\ea83";
-            }
-            .dc-icon-facebook:before {
-                content: "\\ea91";
-            }
-            .dc-icon-twitter:before {
-                content: "\\ea96";
-            }
-            .dc-icon-youtube:before {
-                content: "\\ea9d";
-            }
-            .dc-icon-github:before {
-                content: "\\eab0";
-            }
-            .dc-icon-linkedin:before {
-                content: "\\eac9";
-            }
-        `;
-    }
+  constructor() {
+    super();
+    const iconContainer = document.createElement('div');
+    const shadow = this.attachShadow({ mode: 'open' });
+    const style = document.createElement('style');
+    const iconName = this.getAttribute('icon-name');
+    const iconColor = this.getAttribute('icon-color');
+    const template = document.getElementById(`icon-${iconName}`);
+    const templateContent = template.content;
+    const templateElement = templateContent.firstElementChild;
+    const templateElementClone = template.content.cloneNode(true);
+    templateElement.setAttribute('id', 's2');
+    iconContainer.append(templateElementClone, templateElement);
+    iconContainer.setAttribute('class', 'icon');
+    iconContainer.setAttribute('tabindex', '0');
+    style.textContent = `
+      :host {
+        width: 5.5rem;
+        height: 5.5rem;
+        text-align: initial;
+        margin: auto;
+      }
+      :host(:hover) {
+        cursor: pointer;
+      }
+      :host(:hover) #s2 {
+        opacity: 1;
+        clip-path: circle(120% at bottom);
+        fill: ${iconColor};
+      }
+      .icon {
+        position: relative;
+      }
+      svg {
+        position: absolute;
+        padding: 0.3rem;
+        fill: #424246;
+      }
+      #s2 {
+        opacity: 0;
+        clip-path: circle(0% at bottom);
+        transition: clip-path 2s,  opacity 2.5s;
+      }`;
+    shadow.appendChild(style);
+    shadow.appendChild(iconContainer);
   }
-
-  customElements.define('dc-icon', DCIcon);
+}
+customElements.define('dc-icon', DCIcon);
